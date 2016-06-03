5b6d40596ec3c8d88375656266962aa1a39a6ad4
zhangchu1994 <369648332@qq.com>
2016-06-03 15:08:06 +0800

    写代码！！！！！！！

--- 其他Demo/LuaSpaceShoot-master/Assets/Lua/SpaceShoot/BoundaryCube.lua
+++
@@ -1,8 +0,0 @@
-BoundaryCube = {}
-
-local this = BoundaryCube;
---游戏中的边界碰撞检测逻辑（子弹或者Cube离开游戏界面是销毁功能
-function this.OnTriggerExit(other)
-	print("OnTriggerExit(other");
-	this.gameObject.Destroy(other.gameObject);
-end
