55c5efe2decb3f5f5c9b5ba0464f0ab86da035b4
zhangchu1994 <369648332@qq.com>
2016-07-06 13:59:39 +0800

    写代码！！！！！！！

---
+++ 其他Demo/0_Project/COOP Action Game Kit1.1/Assets/COOP Action Game Kit/_Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,508 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+	[System.Serializable]
+	public class playerDetection {
+		public float detectionRange;
+		public float detectionInterval;
+		[HideInInspector]
+		public bool detectionOnlyIfPlayerInSight=false;
+	}
+	
+	[System.Serializable]
+	public class Attack {
+		public EnemyAttack enemyAttack;
+		public float attackAtAngle=50f;
+		public float attackAtMinRange=4f;
+		public float attackAtMaxRange=4f;
+		public float attackInterval=0.8f;
+		public bool rotateDuringAttack=false;
+		public bool moveDuringAttack=false;
+	}
+	
+	[System.Serializable]
+	public class Evade {
+		public bool canEvade;
+		public float evadeRange;
+		public float evadeOnHealth;
+		public float evadeInterval;
+	}
+	
+	[System.Serializable]
+	public class Hurt {
+		public bool stopOnHurt=true;
+		public float recoverDuration=0.3f;
+	}
+	
+	[System.Serializable]
+	public class Stun {
+		public bool canBeStunned;
+		public float recoverDuration;
+	}
+
+public class EnemyAI : MonoBehaviour {
+
+	public enum State { Roam, Idle, Chase, Evade, Attack, Hurt, Stun, Dead }
+	
+	public playerDetection PlayerDetectionSettings;
+	public Attack AttackSettings;
+	public Evade EvadeSettings;
+	public Hurt HurtSettings;
+	public Stun StunSettings;
+	
+	public float ChoiceMakingInterval;
+	
+	public bool canRoamAround=true;
+	public bool Log=true;
+	
+	[HideInInspector]
+	public PlayerController playerTarget;
+	
+	[HideInInspector]
+	public characterMove cM;
+	[HideInInspector]
+	public characterHealth cH;
+	
+	float TimeOfHurt, TimeOfRandomAngleChange,TimeOfActionChoice,choiceInterval,TimeOfAttack,TimeOfPlayerDetect;
+	
+	[HideInInspector]
+	public bool attacking=false;
+	
+	float h;
+	float v;
+	
+	State state = State.Idle;
+	
+	// Use this for initialization
+	public virtual void Start () {
+		if(AttackSettings.enemyAttack)
+			AttackSettings.enemyAttack.eAI = this;
+		cM=GetComponent<characterMove>();
+		cH=GetComponent<characterHealth>();
+		choiceInterval=ChoiceMakingInterval;
+	}
+	
+	public virtual void OnEnable() {
+		if(!cM)
+			cM=GetComponent<characterMove>();
+		else if(cM.anim)
+			cM.anim.SetBool("Dead",false);
+		TimeOfRandomAngleChange = Time.time;
+		TimeOfActionChoice = Time.time;
+		TimeOfAttack = Time.time;
+		TimeOfPlayerDetect = Time.time;
+		TimeOfHurt = Time.time;
+	}	
+	
+	void OnGUI () {
+		if (!Log)
+			return;
+        GUI.Label (new Rect (0,0,400,50), "State "+state);
+    }
+	
+	void FixedUpdate () {	
+		var newState = UpdateState();
+		
+		if (newState != state)
+			SetState(newState);
+	}
+	
+	public virtual void MakeChoice() {
+		if(TimeSince(TimeOfActionChoice,choiceInterval)) {
+			
+		}
+	}
+	
+	public virtual void SetState(State newState)
+	{
+		state = newState;
+		
+		switch (state)
+		{
+			case State.Roam:
+				SetRoam();
+				break;
+	
+			case State.Idle:
+				SetIdle();
+				break;
+	
+			case State.Chase:
+				SetChase();
+				break;
+			
+			case State.Evade:
+				SetEvade();
+				break;
+			
+			case State.Attack:
+				SetAttack();
+				break;
+				
+			case State.Hurt:
+				SetHurt();
+				break;
+				
+			case State.Stun:
+				SetStun();
+				break;
+				
+			case State.Dead:
+				SetDead();
+				break;
+		}
+	}
+
+	public virtual void SetRoam() {
+		dLog("State mudou para Roam");
+	}
+	
+	public virtual void SetIdle() {
+		dLog("State mudou para Idle.");
+	}
+	
+	public virtual void SetChase() {
+		dLog("State mudou para Chase");
+	}
+	
+	public virtual void SetEvade() {
+		dLog("State mudou para Evade");
+	}
+	
+	public virtual void SetAttack() {
+		dLog("State mudou para Attack");
+	}
+	
+	public virtual void SetHurt() {
+		dLog("State mudou para Hurt");
+		if(cM.anim)
+			cM.anim.Play("Hurt",0);
+	}
+	
+	public virtual void SetStun() {
+		dLog("State mudou para Stun");
+	}
+	
+	public virtual void SetDead() {
+		dLog("State mudou para Dead");
+			cM.anim.SetBool("Dead",true);
+	}
+	
+
+
+	public virtual State UpdateState()
+	{
+		switch (state)
+		{
+			
+			case State.Roam:
+				var RoamStateResult = RoamState();
+				if(RoamStateResult!=state)
+					return RoamStateResult;
+				break;
+			
+			case State.Idle:
+				var IdleStateResult = IdleState();
+				if(IdleStateResult!=state)
+					return IdleStateResult;
+				break;
+			
+			case State.Chase:
+				var ChaseStateResult = ChaseState();
+				if(ChaseStateResult!=state)
+					return ChaseStateResult;
+				break;
+				
+			case State.Evade:
+				var EvadeStateResult = EvadeState();
+				if(EvadeStateResult!=state)
+					return EvadeStateResult;
+				break;
+				
+			case State.Attack:
+				var AttackStateResult = AttackState();
+				if(AttackStateResult!=state)
+					return AttackStateResult;
+				break;
+			
+			case State.Hurt:
+				var HurtStateResult = HurtState();
+				if(HurtStateResult!=state)
+					return HurtStateResult;
+				break;
+				
+			case State.Stun:
+				var StunStateResult = StunState();
+				if(StunStateResult!=state)
+					return StunStateResult;
+				break;
+				
+			case State.Dead:
+				var DeadStateResult = DeadState();
+				if(DeadStateResult!=state)
+					return DeadStateResult;
+				break;
+		}
+		
+		return state;
+	}
+	
+	public virtual State RoamState() {
+		if(TimeSince(TimeOfRandomAngleChange,2f)) {
+			StartCoroutine(MoveFloatH(Random.Range(-1.5f,1.5f),2f));
+			StartCoroutine(MoveFloatV(Random.Range(-1.5f,1.5f),2f));
+			
+			TimeOfRandomAngleChange=Time.time;
+		}
+		
+		if(TimeSince(TimeOfPlayerDetect,PlayerDetectionSettings.detectionInterval)) {
+			var pTarget = GetClosestPlayer(PlayerDetectionSettings.detectionRange , PlayerDetectionSettings.detectionOnlyIfPlayerInSight);
+			if(pTarget) {
+				playerTarget = pTarget;
+				return State.Chase;
+			}
+		}
+		
+		if(TimeSince(TimeOfActionChoice,ChoiceMakingInterval)) {
+			if(Random.value<0.3f&&canRoamAround)
+				return State.Idle;
+		}
+		
+		cM.TryMovement(h,v,true);
+		
+		return state;
+	}
+	
+	public virtual State IdleState() {
+	
+		if(TimeSince(TimeOfActionChoice,ChoiceMakingInterval)) {
+			TimeOfActionChoice=Time.time;
+			if(Random.value<0.3f&&canRoamAround)
+				return State.Roam;
+		}
+		
+		if(TimeSince(TimeOfPlayerDetect,PlayerDetectionSettings.detectionInterval)) {
+			TimeOfPlayerDetect=Time.time;
+			var pTarget = GetClosestPlayer(PlayerDetectionSettings.detectionRange , PlayerDetectionSettings.detectionOnlyIfPlayerInSight);
+			if(pTarget) {
+				playerTarget = pTarget;
+				return State.Chase;
+			}
+		}
+	
+		return state;
+	}
+  
+	public virtual State ChaseState() {
+		//Vixe sumiu o Player quero outro
+		if(!playerTarget||playerTarget.cH.Dead) {
+			var pTarget = GetClosestPlayer(PlayerDetectionSettings.detectionRange*3);
+			//Se achei outro seta, senão vira idle
+			if(pTarget)
+				playerTarget = pTarget;
+			else
+				return State.Idle;
+		}
+		
+		if(TimeSince(TimeOfPlayerDetect,PlayerDetectionSettings.detectionInterval)) {
+			TimeOfPlayerDetect=Time.time;
+			if(transform.DistanceTo(playerTarget.transform)<AttackSettings.attackAtMinRange && cM.body.transform.isInFront(playerTarget.transform,AttackSettings.attackAtAngle))
+				return State.Attack;
+		}
+		
+		Vector3 dir = transform.DirectionTo(playerTarget.transform);
+		cM.TryMovement(dir.x,dir.z,true);
+		
+		return state;
+	}
+	
+	public virtual State EvadeState() {
+	
+		return state;
+	}
+	
+	public virtual State AttackState() {
+		if(attacking && AttackSettings.rotateDuringAttack)
+			RotateToPlayer();
+			
+		if(attacking && AttackSettings.moveDuringAttack)
+			MoveToPlayer();
+			
+		if(attacking)
+			return state;
+			
+		if(!playerTarget||playerTarget.cH.Dead)
+			return State.Chase;
+		
+		if(TimeSince(TimeOfPlayerDetect,PlayerDetectionSettings.detectionInterval)) {
+			TimeOfPlayerDetect=Time.time;
+			if(transform.DistanceTo(playerTarget.transform)>AttackSettings.attackAtMaxRange || !cM.body.transform.isInFront(playerTarget.transform,AttackSettings.attackAtAngle))
+				return State.Chase;
+		}
+		
+		if(!AttackSettings.enemyAttack)
+			return State.Chase;
+			
+		if(TimeSince(TimeOfAttack,AttackSettings.attackInterval)) {
+			TimeOfAttack=Time.time;
+			AttackSettings.enemyAttack.Attack(transform.DirectionTo(playerTarget.transform));
+		}
+		
+		RotateToPlayer();
+		MoveToPlayer();
+		
+		return state;
+	}
+	
+	public virtual State HurtState() {
+		if(TimeSince(TimeOfHurt,HurtSettings.recoverDuration))
+			return State.Chase;
+	
+		return state;
+	}
+	
+	public virtual State StunState() {
+	
+		return state;
+	}
+	
+	public virtual State DeadState() {
+	
+		return state;
+	}
+	
+	public virtual void RotateToPlayer() {
+		Vector3 dir = transform.DirectionTo(playerTarget.transform);
+		cM.RotateTo(dir,false);
+	}
+	
+	public virtual void MoveToPlayer() {
+		Vector3 dir = transform.DirectionTo(playerTarget.transform);
+		cM.TryMovement(dir.x,dir.z,true);
+	}
+	
+	public virtual void OnDamage(Transform attacker) {
+		if(!playerTarget)
+			playerTarget=attacker.GetComponent<PlayerController>();
+			
+		if(playerTarget.transform!=attacker && Random.value<0.4f)
+			playerTarget=attacker.GetComponent<PlayerController>();
+		
+		if(!HurtSettings.stopOnHurt)
+			return;
+		
+		TimeOfHurt = Time.time;
+		SetState(State.Hurt);
+	}
+	
+	public virtual void OnDeath() {
+		SetState(State.Dead);
+	}
+	
+	public virtual PlayerController GetClosestPlayer(float range, bool LOS=false) {
+		if(!CamManager.instance)
+			return null;
+		
+		float distance=range+0.1f;
+		PlayerController ClosestPlayer = null;
+		
+		foreach (PlayerController player in CamManager.instance.Players) {
+			if(player.cH.Dead)
+				continue;
+			if(LOS) {
+				if(!HaveLineOfSight(player.transform))
+					continue;
+			}
+			float myDistance = transform.DistanceTo(player.transform);
+			if(myDistance<distance){
+				distance=myDistance;
+				ClosestPlayer=player;
+			}
+    	}
+    	
+    	return ClosestPlayer;
+	}
+	
+	public bool HaveLineOfSight (Transform Other) {
+		RaycastHit hit;
+		var rayDirection = Other.position - transform.position;
+		
+		int layerToIgnore = 1 << gameObject.layer;
+        layerToIgnore = ~layerToIgnore;
+ 
+		if (Physics.Raycast (transform.position, rayDirection, out hit, PlayerDetectionSettings.detectionRange, layerToIgnore)) {
+	        if (hit.transform == Other) {
+	            return true;
+	        } else {
+	            return false;
+	        }
+	    }
+	    return false;
+	}
+	
+	public virtual List<PlayerController> GetPlayersInRange(float range) {
+		if(!CamManager.instance)
+			return null;
+		
+		List<PlayerController> Players = new List<PlayerController>();
+		
+		foreach (PlayerController player in CamManager.instance.Players) {
+			if(player.cH.Dead)
+				continue;
+			float myDistance = transform.DistanceTo(player.transform);
+			if(myDistance<range){
+				Players.Add(player);
+			}
+    	}
+    	
+    	return Players;
+	}
+	
+	IEnumerator MoveFloatH(float target, float duration)
+	{
+		float elapsed = 0;
+		var start = h;
+		var range = target - start;
+		//Debug.Log("duration "+duration+" elapsed "+elapsed );
+		while (elapsed < duration)
+		{
+			//Debug.Log("duration "+duration+" elapsed "+elapsed );
+			elapsed = Mathf.MoveTowards(elapsed, duration, Time.deltaTime);
+			h = start + range *  Ease.ExpoIn(elapsed / duration);
+			yield return 0;
+		}
+		h = target;
+	}
+	
+	IEnumerator MoveFloatV(float target, float duration)
+	{
+		float elapsed = 0;
+		var start = v;
+		var range = target - start;
+		//Debug.Log("duration "+duration+" elapsed "+elapsed );
+		while (elapsed < duration)
+		{
+			//Debug.Log("duration "+duration+" elapsed "+elapsed );
+			elapsed = Mathf.MoveTowards(elapsed, duration, Time.deltaTime);
+			v = start + range * Ease.ExpoIn(elapsed / duration);
+			yield return 0;
+		}
+		v = target;
+	}
+	
+	bool TimeSince(float thisTime, float interval) {
+		if(thisTime + interval > Time.time) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	public void dLog(string log) {
+		if(!Log)
+			return;
+			
+		Debug.Log(log);
+	}
+}
---
+++ 其他Demo/0_Project/COOP Action Game Kit1.1/Assets/COOP Action Game Kit/_Scripts/Misc/Menu/CheatingRoom.cs
@@ -0,0 +1,219 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheatingRoom : MonoBehaviour {
+
+	public RangedWeapon wp;
+	public characterMove cM;
+	public CamManager cF;
+	
+	public CheatingRoomInput ci;
+	
+	public Transform RangedEnemy;
+	public Transform MeleeEnemy;
+	
+	public GUIStyle myStyle;
+	
+
+	public Font MyFont;
+	
+	bool ShowCheats=true;
+	
+	bool Init=false;
+	
+	void Start() {
+		StartCoroutine(LateStart());
+		ci=GetComponent<CheatingRoomInput>();
+	}
+	void Update () {
+		if(!ShowCheats||!Init)
+			return;
+			
+		if(ci.height.Length>0)
+			cF.height = int.Parse(ci.height);
+		if(ci.distance.Length>0)
+			cF.distance = int.Parse(ci.distance);
+		
+		if(ci.acceleration.Length>0)
+			cM.acceleration = float.Parse(ci.acceleration);
+		if(ci.deceleration.Length>0)
+			cM.deceleration = float.Parse(ci.deceleration);
+		if(ci.moveSpeed.Length>0)
+			cM.currentMoveSpeed = float.Parse(ci.moveSpeed);
+		if(ci.jumpForce.Length>0)
+			cM.jumpForce = float.Parse(ci.jumpForce);
+		if(ci.gravity.Length>0)
+			cM.gravity = float.Parse(ci.gravity);
+		
+		if(ci.projectileSpeed.Length>0)	
+			wp.projectileSettings.projectileSpeed = float.Parse(ci.projectileSpeed);
+		if(ci.projectileAngleVariation.Length>0)
+			wp.projectileSettings.projectileAngleVariation = float.Parse(ci.projectileAngleVariation);
+		if(ci.bulletsPerShot.Length>0)
+			wp.ShootSettings.bulletsPerShot = int.Parse(ci.bulletsPerShot);
+		if(ci.weaponKick.Length>0)
+			wp.ShootSettings.weaponKick = float.Parse(ci.weaponKick);
+		if(ci.weaponKickDuration.Length>0)
+			wp.ShootSettings.weaponKickDuration = float.Parse(ci.weaponKickDuration);
+		if(ci.ReloadInterval.Length>0)
+			wp.AmmoSettings.ReloadInterval = float.Parse(ci.ReloadInterval);
+		
+		if(ci.AttackCooldown.Length>0)	
+			wp.AttackCooldown = float.Parse(ci.AttackCooldown);
+	}
+	
+	IEnumerator LateStart() {
+		yield return new WaitForSeconds(0.1f);
+		
+		wp=(RangedWeapon)cM.GetComponent<PlayerController>().weaponManager.currentWeapon;
+		
+		ci.height=cF.height.ToString();
+		ci.distance=cF.distance.ToString();
+		
+		ci.acceleration = cM.acceleration.ToString();
+		ci.deceleration = cM.deceleration.ToString();
+		ci.moveSpeed = cM.currentMoveSpeed.ToString();
+		ci.jumpForce = cM.jumpForce.ToString();
+		ci.gravity = cM.gravity.ToString();
+		
+		ci.projectileSpeed = wp.projectileSettings.projectileSpeed.ToString();
+		ci.projectileAngleVariation = wp.projectileSettings.projectileAngleVariation.ToString();
+		ci.bulletsPerShot = wp.ShootSettings.bulletsPerShot.ToString();
+		ci.weaponKick = wp.ShootSettings.weaponKick.ToString();
+		ci.weaponKickDuration = wp.ShootSettings.weaponKickDuration.ToString();
+		ci.ReloadInterval = wp.AmmoSettings.ReloadInterval.ToString();
+		ci.AttackCooldown = wp.AttackCooldown.ToString();
+		
+		wp.AmmoSettings.MaxAmmo=500;
+		wp.AmmoSettings.CurrentAmmo=500;
+		
+		Init=true;
+		yield return 0;
+	}
+	
+	void OnGUI() {
+		if(!Init)
+			return;
+			
+		GUIStyle myStyle = new GUIStyle();
+		myStyle.fontSize=13;
+		GUIStyle myStyleInput = new GUIStyle();
+		myStyleInput.fontSize=13;
+		GUIStyle myStyleH = new GUIStyle();
+		myStyleH.fontSize=22;
+		GUIStyle myStyleV = new GUIStyle();
+		myStyleV.fontSize=16;
+		myStyleV.normal.textColor=Color.white;
+	    GUI.skin.font = MyFont;
+	    
+	    if(DrawButtonWithShadow(new Rect(10,Screen.height - 50, 100, 50),new GUIContent("Main Menu"),myStyleV,0.5f,new Vector2(3,3)))
+	            Application.LoadLevel("MainMenu");
+	    
+	    if(!ShowCheats) {
+			if(DrawButtonWithShadow(new Rect(Screen.width - 125,Screen.height - 50, 100, 50),new GUIContent("Show Cheats"),myStyleV,0.5f,new Vector2(3,3)))
+	            ShowCheats=!ShowCheats;
+	        return;
+		}
+
+		int yspace = 10;
+	    DrawShadow(new Rect(10, yspace, 150, 100), new GUIContent("Camera"),myStyleH,Color.white,Color.black,new Vector2(2,2));
+	    
+	    int regularspacing=30;    
+	    
+	    yspace += regularspacing;
+	    DrawShadow(new Rect(10, yspace, 150, 100), new GUIContent("Height"),myStyle,Color.white,Color.black,new Vector2(2,2));
+	    DrawShadow(new Rect(95, yspace, 150, 100), new GUIContent("Distance"),myStyle,Color.white,Color.black,new Vector2(2,2));
+	    
+		yspace += regularspacing;
+	    DrawShadow(new Rect(10, yspace, 150, 100), new GUIContent("Player"),myStyleH,Color.white,Color.black,new Vector2(2,2));
+	    yspace += regularspacing;
+	    DrawShadow(new Rect(10, yspace, 150, 100), new GUIContent("Accel"),myStyle,Color.white,Color.black,new Vector2(2,2));
+	    DrawShadow(new Rect(95, yspace, 150, 100), new GUIContent("Decel"),myStyle,Color.white,Color.black,new Vector2(2,2));
+	    
+		yspace += regularspacing;
+	    
+	    DrawShadow(new Rect(10, yspace, 150, 100), new GUIContent("Speed"),myStyle,Color.white,Color.black,new Vector2(2,2));
+	    DrawShadow(new Rect(95, yspace, 150, 100), new GUIContent("Jump"),myStyle,Color.white,Color.black,new Vector2(2,2));
+	    DrawShadow(new Rect(180, yspace, 150, 100), new GUIContent("Gravity"),myStyle,Color.white,Color.black,new Vector2(2,2));
+	    
+		yspace += regularspacing;
+	    DrawShadow(new Rect(10, yspace, 150, 100), new GUIContent("Weapon"),myStyleH,Color.white,Color.black,new Vector2(2,2));
+	    yspace += regularspacing;
+	    DrawShadow(new Rect(10, yspace, 150, 100), new GUIContent("ProjSpeed"),myStyle,Color.white,Color.black,new Vector2(2,2));
+	    DrawShadow(new Rect(140, yspace, 150, 100), new GUIContent("AngleVariation"),myStyle,Color.white,Color.black,new Vector2(2,2));
+	    yspace += regularspacing;
+
+
+	    DrawShadow(new Rect(10, yspace, 150, 100), new GUIContent("BulletsP/Shot"),myStyle,Color.white,Color.black,new Vector2(2,2));
+	    DrawShadow(new Rect(140, yspace, 150, 100), new GUIContent("ReloadTime"),myStyle,Color.white,Color.black,new Vector2(2,2));
+	    yspace += regularspacing;
+	
+	    DrawShadow(new Rect(10, yspace, 150, 100), new GUIContent("WeaponKick"),myStyle,Color.white,Color.black,new Vector2(2,2));
+	    DrawShadow(new Rect(140, yspace, 150, 100), new GUIContent("KickDuration"),myStyle,Color.white,Color.black,new Vector2(2,2));
+	    
+		yspace += regularspacing;
+	    DrawShadow(new Rect(10, yspace, 150, 100), new GUIContent("Attack Interval"),myStyle,Color.white,Color.black,new Vector2(2,2));
+	    
+	    
+	    
+	    	    
+	    if(DrawButtonWithShadow(new Rect(Screen.width - 190, 10, 100, 50),new GUIContent("Spawn Ranged Enemy"),myStyleV,0.5f,new Vector2(3,3)))
+            SpawnRanged();
+            
+        if(DrawButtonWithShadow(new Rect(Screen.width - 190, 60, 100, 50),new GUIContent("Spawn Melee Enemy"),myStyleV,0.5f,new Vector2(3,3)))
+            SpawnMelee();
+        
+        if(DrawButtonWithShadow(new Rect(Screen.width - 125,Screen.height - 50, 100, 50),new GUIContent("Hide cheats"),myStyleV,0.5f,new Vector2(3,3)))
+            ShowCheats=!ShowCheats;
+    }
+    
+    void SpawnRanged() {
+    	Vector3 SpawnPosition = cM.gameObject.transform.position + (Random.onUnitSphere*25);
+    	//ctor3 SpawnPosition = new Vector3(cM.gameObject.transform.position.x+Random.Range(-1,1)*20,2, cM.gameObject.transform.position.z+Random.Range(-1,1)*20);
+    	SpawnPosition.y=2;
+    	
+    	Instantiate(RangedEnemy, SpawnPosition, Quaternion.identity);
+    }
+    
+    void SpawnMelee() {
+    	Vector3 SpawnPosition = cM.gameObject.transform.position + (Random.onUnitSphere*25);
+    	//ctor3 SpawnPosition = new Vector3(cM.gameObject.transform.position.x+Random.Range(-1,1)*20,2, cM.gameObject.transform.position.z+Random.Range(-1,1)*20);
+    	SpawnPosition.y=2;
+    	
+    	Instantiate(MeleeEnemy, SpawnPosition, Quaternion.identity);
+    }
+    
+    public bool DrawButtonWithShadow(Rect r, GUIContent content, GUIStyle style, float shadowAlpha, Vector2 direction)
+        {
+            GUIStyle letters = new GUIStyle(style);
+            letters.normal.background = null;
+            letters.hover.background = null;
+            letters.active.background = null;
+ 
+            bool result = GUI.Button(r, content, style);
+ 
+            Color color = r.Contains(Event.current.mousePosition) ? letters.hover.textColor : letters.normal.textColor;
+ 
+            DrawShadow(r, content, letters, color, new Color(0f, 0f, 0f, shadowAlpha), direction);
+ 
+            return result;
+        }
+    
+    public void DrawShadow(Rect rect, GUIContent content, GUIStyle style, Color txtColor, Color shadowColor,
+                                        Vector2 direction)
+        {
+            GUIStyle backupStyle = style;
+ 
+            style.normal.textColor = shadowColor;
+            rect.x += direction.x;
+            rect.y += direction.y;
+            GUI.Label(rect, content, style);
+ 
+            style.normal.textColor = txtColor;
+            rect.x -= direction.x;
+            rect.y -= direction.y;
+            GUI.Label(rect, content, style);
+ 
+            style = backupStyle;
+        }
+}
