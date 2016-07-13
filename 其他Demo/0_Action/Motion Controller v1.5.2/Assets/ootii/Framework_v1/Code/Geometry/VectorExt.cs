5f66ed1a847054dc305c37f4a144c016ad17ab32
zhangchu1994 <369648332@qq.com>
2016-07-13 15:37:58 +0800

    写代码！！！！！！！

---
+++ 其他Demo/0_Action/Motion Controller v1.5.2/Assets/ootii/Framework_v1/Code/Geometry/VectorExt.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace com.ootii.Geometry
+{
+    /// <summary>
+    /// Extension for the standard Vector3 that allows us to add functions
+    /// </summary>
+    public static class VectorExt
+    {
+    }
+}
