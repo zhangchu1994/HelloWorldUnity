f457af106e9b32740a9f4820f354d6185e339a82
zhangchu1994 <369648332@qq.com>
2017-02-27 14:40:30 +0800

    添加UI特效

---
+++ LuaFramework_UGUI-master/Assets/Scripts/TestClass.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GlobalGame 
+{
+	public class TestClass : MonoBehaviour 
+	{
+		public GameObject m_PS;
+
+		// Use this for initialization
+		void Start () 
+		{
+			Global.ChangeParticleScale (m_PS, 15);
+		}
+		
+		// Update is called once per frame
+		void Update () 
+		{
+			
+		}
+	}
+}
